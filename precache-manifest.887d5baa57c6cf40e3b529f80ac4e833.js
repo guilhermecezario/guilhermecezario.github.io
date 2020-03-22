d54653d72872cd07f69dd34c7f14b60c3cebe4db
guilhermeCezario <cezariogui@outlook.com>
2020-03-22 02:13:31 -0300

    Updates

---
+++ precache-manifest.887d5baa57c6cf40e3b529f80ac4e833.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5b7b64e5200ea8113a3902b525a75f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efcc57bfe1c534d5d038",
+    "url": "/static/css/main.ad0c3232.chunk.css"
+  },
+  {
+    "revision": "bf75d975af2f2e0d0d7a",
+    "url": "/static/js/2.7f19d07b.chunk.js"
+  },
+  {
+    "revision": "e468b20662ce7acec401182a9064b329",
+    "url": "/static/js/2.7f19d07b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efcc57bfe1c534d5d038",
+    "url": "/static/js/main.07c15baf.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "fbe2bc37ddc0d1aab834c6838003502b",
+    "url": "/static/media/Procrastinating.fbe2bc37.svg"
+  },
+  {
+    "revision": "2ef81c65794c3f5b134b39f30d15a8fc",
+    "url": "/static/media/github.2ef81c65.svg"
+  },
+  {
+    "revision": "c733b26a2c8178914e17cf7d94ee81a6",
+    "url": "/static/media/linkedin.c733b26a.svg"
+  },
+  {
+    "revision": "5f257e78f23d012de4311d5521282520",
+    "url": "/static/media/logo.5f257e78.svg"
+  },
+  {
+    "revision": "b265fe90024fef56a040474a3e21e5e9",
+    "url": "/static/media/profile.b265fe90.jpg"
+  },
+  {
+    "revision": "eab44233f4c3d2f17782ab934bb406f9",
+    "url": "/static/media/twitter.eab44233.svg"
+  }
+]);
